ca781538869ff16a4b4c76343c9bc730cbaf189d
Denis Streitmatter <streitmatter@informatik.uni-leipzig.de>
2021-07-11 10:31:21 +0200

    Adds bleu eval system

--- README.md
+++ README.md
@@ -3,3 +3,26 @@
 These are resources generated for the Seminar Citicen Science at the University of Leipzig.
 
 All data stem from https://harry-potter.fandom.com/de/wiki/Originalnamen_und_-bezeichnungen and is merged directly in one csv. The data contains various named entities and things from the Harry Potter universe, like charms, potions, figures of speech, games and many more.
+
+## Installation
+- create virtual environment with *virtualenv*
+    - `python3 -m virtualenv env`
+- activate virtual environment
+    - `source env/bin/activate`
+- download required packages
+    - `pip3 -r requirements.txt`
+- download spacy german core:
+    - `python -m spacy download de_core_news_sm`
+
+## Translations
+
+German translations are available in the directory `german_translations` and are grouped by their author and named after the chapter (e.g. `german_translations/Jost/15.txt`)
+
+
+## BLEU evaluation
+
+To get the bleu score of a chapter the chapter needs to be identified with the id and the text to be evaluated needs to be in a file like `./example/dir/text.txt`
+
+```
+python bleu_eval.py -c <id> -t ./example/dir/text.txt
+```
---
+++ bleu_eval.py
@@ -0,0 +1,96 @@
+import os
+import argparse
+from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
+import json
+import spacy
+
+nlp = spacy.load("de_core_news_sm")
+
+def get_chapter_files(chapter_id: int, basedir: str = "./german-translations"):
+
+    results = {}
+
+    for author in os.listdir(basedir):
+
+        author_path = os.path.join(basedir, author)
+
+        if not os.path.isdir(author_path): continue
+
+        chapter_path = os.path.join(author_path, str(chapter_id) + ".txt")
+
+        if not os.path.isfile(chapter_path): continue
+
+        results[author] = chapter_path
+
+    return results
+
+def get_file_content(path):
+
+    res = []
+
+    with open(path) as f:
+        s = f.read()
+        s = s.replace("\n", " ")
+        doc = nlp(s)
+
+    for token in doc: 
+        if not token.is_stop and not token.is_punct and not token.is_space:
+            res.append(token.text)
+    
+    return res
+
+
+def calculate_bleu(results_mapping, target_file):
+
+    authors, files = zip(*results_mapping.items())
+
+    # results = file_bleu(files, target_file)
+
+    # author_results = zip(authors, results)
+
+    # print(json.dumps(author_results, indent=4))
+
+    chencherry = SmoothingFunction()
+
+    references = [get_file_content(path) for path in files]
+
+    with open("reference.json", "w+") as output_file:
+        json.dump(references[0], output_file, indent=4)
+
+    target_content = get_file_content(target_file)
+
+    with open("hypothesis.json", "w+") as output_file:
+        json.dump(target_content, output_file, indent=4)
+
+    for i, r in enumerate(references):
+        bleu = corpus_bleu([[r]], [target_content], smoothing_function=chencherry.method1)
+        author = authors[i]
+        print(f"{author}: {bleu=}")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="A tool for evaluating the BLEU score of the german HPMOR translation. WORK IN PROGRESS"
+    )
+    parser.add_argument(
+        "-c",
+        "--chapter_reference",
+        type=int,
+        help="An integer to indicate the chapter to be compared to.",
+        required=True,
+    )
+    parser.add_argument(
+        "-t",
+        "--target_file",
+        type=str,
+        help="A path to a file which should be evaluated",
+        required=True,
+    )
+    args = parser.parse_args()
+
+
+    res = get_chapter_files(args.chapter_reference) 
+
+
+    calculate_bleu(res, args.target_file)
